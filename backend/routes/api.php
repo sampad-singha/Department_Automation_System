6fbeb5242931ed03b85033c9204871c82e3843ec
Saiful Islam sohan <saifulislam.cse.pust@gmail.com>
2025-02-13 21:55:59 +0600

    update  api route name

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'userLogin'], function () {
     
-    Route::post('/LoginUser',[UserAuthController::class, 'login'])->middleware('throttle:userLogin');
-    Route::post('/LogoutUser',[UserAuthController::class, 'UserLogOut'])->middleware('auth:sanctum');
+    Route::post('/Login',[UserAuthController::class, 'login'])->middleware('throttle:userLogin');
+    Route::post('/Logout',[UserAuthController::class, 'UserLogOut'])->middleware('auth:sanctum');
     Route::post('/rest-password',[UserAuthController::class,'changePassword'])->middleware('auth:sanctum');
     
 });
