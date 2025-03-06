2f1f516ca3bcd923b40179e6810cbb55dd2a4c47
Saiful Islam sohan <saifulislam.cse.pust@gmail.com>
2025-03-06 13:41:31 +0600

    remove controller name

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\api\NoticeController;
 use App\Http\Controllers\Api\UserAuthController;
 use App\Http\Controllers\Api\PasswordResetController;
-use App\Http\Controllers\Api\ResetPasswordController;
-use App\Http\Controllers\Api\ChangePasswordController;
 use App\Http\Controllers\Api\ForgetPasswordController;
 use App\Http\Controllers\Api\ShowNoticeController;
 
